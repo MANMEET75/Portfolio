42703cd173ac813b7f67f84f3f39d2790cfa8dec
MANMEET SINGH (AWM) <samanmeet85@gmail.com>
2023-05-04 02:55:13 +0530

    working on cloud part

--- js/main.js
+++ js/main.js
@@ -472,32 +472,5 @@ const swiper = new Swiper(".swiper-slider", {
   
 
 
-  // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml6 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml6 .letter',
-    translateY: ["1.1em", 0],
-    translateZ: 0,
-    duration: 650,
-    delay: (el, i) => 50 * i
-  }).add({
-    targets: '.ml6',
-    opacity: 0,
-    duration: 1700,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
 
 
-
-  $(document).ready(function() {
-    $('#bt1').click(function() {
-        $('#fr1').attr('action',
-                       'mailto:samanmeet85@gmail.com?subject=' +
-                       $('#tb1').val() + '&body=' + $('#tb2').val());
-        $('#fr1').submit();
-    });
-})
