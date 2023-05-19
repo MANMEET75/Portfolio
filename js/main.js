698f6afc5d5a37933b899027ba5e09f204740ee2
MANMEET SINGH (AWM) <samanmeet85@gmail.com>
2023-05-20 01:26:15 +0530

    adding the jquery animation in a website

--- js/main.js
+++ js/main.js
@@ -232,9 +232,10 @@
                   var effect = el.data("animate-effect");
                   if (effect === "fadeIn") {
                     el.addClass("fadeIn ftco-animated");
-                  } else if (effect === "fadeInLeft") {
-                    el.addClass("fadeInLeft ftco-animated");
-                  } else if (effect === "fadeInRight") {
+                  } 
+                  // else if (effect === "fadeInLeft") {
+                    // el.addClass("fadeInLeft ftco-animated");
+                  else if (effect === "fadeInRight") {
                     el.addClass("fadeInRight ftco-animated");
                   } else {
                     el.addClass("fadeInUp ftco-animated");
@@ -487,4 +488,119 @@ $(function () {
 
 
 
+// adding way point offsets code over here
+// var waypoint = new Waypoint({
+//   element: document.getElementById('px-offset-waypoint'),
+//   handler: function(direction) {
+//     notify('I am 20px from the top of the window')
+//   },
+//   offset: 20 
+// })
 
+
+
+$(document).ready(function () {
+  $(".fadeInleft-animate").waypoint(function(direction){
+    $(".fadeInleft-animate").addClass("animated fadeInLeft")
+
+  },{
+    offset:"50%"
+  });
+  $(".fadeInRight-animate").waypoint(function(direction){
+    $(".fadeInRight-animate").addClass("animated fadeInRight")
+
+  },{
+    offset:"50%"
+  });
+  $(".zoomInDown-animate").waypoint(function(direction){
+    $(".zoomInDown-animate").addClass("animated zoomInDown")
+
+  },{
+    offset:"50%"
+  });
+  $(".zoomIn-animate").waypoint(function(direction){
+    $(".zoomIn-animate").addClass("animated zoomIn")
+
+  },{
+    offset:"50%"
+  });
+  $(".fadeInUp-animate").waypoint(function(direction){
+    $(".fadeInUp-animate").addClass("animated fadeInUp")
+
+  },{
+    offset:"50%"
+  });
+  $(".rotateInDownRight-animate").waypoint(function(direction){
+    $(".rotateInDownRight-animate").addClass("animated rotateInDownRight")
+
+  },{
+    offset:"50%"
+  });
+  $(".rotateInDownLeft-animate").waypoint(function(direction){
+    $(".rotateInDownLeft-animate").addClass("animated rotateInDownLeft")
+
+  },{
+    offset:"50%"
+  });
+  $(".flash-animate").waypoint(function(direction){
+    $(".flash-animate").addClass("animated flash")
+
+  },{
+    offset:"50%"
+  });
+
+
+
+  
+
+
+
+
+});
+
+
+
+
+// this is the code over here to add
+//  var contentWayPoint = function () {
+//   var i = 0;
+//   $(".ftco-animate").waypoint(
+//     function (direction) {
+//       if (
+//         direction === "down" &&
+//         !$(this.element).hasClass("ftco-animated")
+//       ) {
+//         i++;
+
+//         $(this.element).addClass("item-animate");
+//         setTimeout(function () {
+//           $("body .ftco-animate.item-animate").each(function (k) {
+//             var el = $(this);
+//             setTimeout(
+//               function () {
+//                 var effect = el.data("animate-effect");
+//                 if (effect === "fadeIn") {
+//                   el.addClass("fadeIn ftco-animated");
+//                 } else if (effect === "fadeInLeft") {
+//                   el.addClass("fadeInLeft ftco-animated");
+//                 } else if (effect === "fadeInRight") {
+//                   el.addClass("fadeInRight ftco-animated");
+//                 } else {
+//                   el.addClass("fadeInUp ftco-animated");
+//                 }
+//                 el.removeClass("item-animate");
+//               },
+//               k * 50,
+//               "easeInOutExpo"
+//             );
+//           });
+//         }, 100);
+//       }
+//     },
+//     { offset: "95%" }
+//   );
+// };
+
+
+
+// contentWayPoint();
